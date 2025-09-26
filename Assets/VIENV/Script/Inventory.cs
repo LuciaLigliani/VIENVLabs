9a80ea384b1f67face3e9e11c3494291602d5599
lucia.ligliani <lucia.ligliani@gstudenti.unicam.it>
2025-09-26 14:08:53 +0000

    Lab 5 set up

--- Assets/VIENV/Script/Inventory.cs
+++ Assets/VIENV/Script/Inventory.cs
@@ -1,21 +1,30 @@
+﻿
 using UnityEngine;
+using System.Collections;
 
-public class Inventory : MonoBehaviour
-{
-    public AudioClip pickupSound;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+public class Inventory : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public int count = 0;
+    public int max = 2;
 
-    void Pickup() {
-		AudioSource.PlayClipAtPoint (pickupSound, transform.position);
+	// Use this for initialization
+	void Start () {
+	
 	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Pickup() {
+		Debug.Log ("Got Pickup");
+		count++;
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(0,0,Screen.width, Screen.height),"Items in inventory:"+count.ToString()+" / "+max.ToString());
+	}
+
 }
+
---
+++ Assets/VIENV/Script/KeyInventory.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Keeps track of the player's possession of a key
+/// </summary>
+public class KeyInventory : Inventory {
+
+    // Texture representing the presence of a Key in the inventory
+    public Texture aTexture;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Return true if key in possession
+    public bool HasKey() {
+        return (count > 0); // Assuming any object in inventory is a key!
+    }
+	
+
+
+	void OnGUI() {
+        // Draw key icon if key in possession
+		if (HasKey()) {
+			GUI.DrawTexture (new Rect (10, 10, 100, 100), aTexture, ScaleMode.ScaleAndCrop, true, 1.0f);
+		}
+	}
+}
